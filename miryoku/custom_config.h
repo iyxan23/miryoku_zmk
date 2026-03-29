a2501afc60c0724deadf5d8cdd43e81ade27ffb1
iyxan23 <github@iyxan.dev>
2026-03-29 17:59:31 +0700

    chore: add custom config (copying from original config)

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,14 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#include <behaviors.dtsi>
+#include <dt-bindings/zmk/bt.h>
+#include <dt-bindings/zmk/keys.h>
+
+&soft_off {
+    hold-time-ms = <3000>;
+};
+
+&soft_off {
+    /delete-property/ split-peripheral-off-on-press;
+};
